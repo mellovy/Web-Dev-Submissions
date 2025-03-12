3c947161688d5a4c58791b7f8f795dd76a9980b4
mellovy <22100684@usc.edu.ph>
2025-03-12 21:56:36 +0800

    Add files via upload

---
+++ MIDTERM_Cabahug/README.txt
@@ -0,0 +1,23 @@
+---- HOW TO USE WEB CALCULATOR ---
+
+Use it just like how you would any in-person calculator.
+
+The web calculator made for this project is a very basic calculator that does
+basic arithmetic. 
+
+See below for operators included.
+
+Operators included in the Web Calculator:
+
+1. (+) Addition
+2. (-) Subtraction
+3. (*) Multiplication
+4. (/) Division
+
+Other functionalities:
+
+- (AC) To delete all inputted numbers
+- (DEL) Serves as a backspace to delete last inputted number
+
+---------------------------------
+
---
+++ MIDTERM_Cabahug/script.js
@@ -0,0 +1,23 @@
+function display(value) {
+    let display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function backspace() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
+
